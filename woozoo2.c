e8fc1ea2d6d6eef1953d87c165773466a881284b
createmain <createmain@gmail.com>
2020-06-05 00:06:58 +0900

    woozoo's first code

---
+++ woozoo2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+
+void main(){
+	int q = 1 + 1;
+	printf("hellow %d", q);
+}
---
+++ woozoo3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int add(int a, int b){
+	return a + b;
+}
+
+void main(){
+	int q = 1 + 1;
+	printf("hellow %d", add(87246, 66576));
+}
